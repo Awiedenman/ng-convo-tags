a8bad25a270d0342b2306b036a6ae7cf1980c664
awiedenman <awiedenman@gmail.com>
2024-08-26 10:56:50 -0600

    Add initial @ symbol detection

--- src/app/components/comment-input/comment-input.component.ts
+++ src/app/components/comment-input/comment-input.component.ts
@@ -17,7 +17,12 @@ export class CommentInputComponent {
     if (typeof this.comment.trim() === 'string') { // null check and type check;
       this.commentSubmitted.emit(this.comment);
       this.comment = '';
-    }
-  }
-
-}
+    };
+  };
+  onInputChange(): void {
+    const symbolTriggerPresent = this.comment.match(/^@/)
+    if (symbolTriggerPresent) {
+      console.log('show user list')
+    };
+  };
+};
